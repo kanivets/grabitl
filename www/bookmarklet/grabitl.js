efd0254238614e2cf6e0dc246a51ca8356aecbbd
dmitry.korniienko <dmitry.korniienko@akqa.com>
2012-10-30 12:54:39 +0200

    Added some backend functionality

---
+++ www/bookmarklet/grabitl.js
@@ -0,0 +1,27 @@
+javascript:function iprl5() {
+	var z=document.createElement('scr'+'ipt');
+	try{
+		if(!document.body)
+			throw(0);
+		
+		document.title='(Saving...) '+document.title;
+		z.setAttribute('src', document.location.protocol+'//www.instapaper.com/j/GvkVfRZYfP0n?u='+encodeURIComponent(document.location.href)+'&t='+(new Date().getTime()));
+		b.appendChild(z);
+	}
+	catch(e){
+		alert('page was not loaded.');
+	}
+}
+iprl5();void(0)
+
+javascript:function grabitl() {
+	if (!document.body) {
+		alert('please wait until page is loaded');
+		return;
+	}
+	var el = document.createElement('scr'+'ipt');
+	el.setAttribute('src', 'http://grabitl/process.php?h=haarer0&p='+encodeURIComponent(document.location.href)+'&t='+(new Date().getTime()));
+	document.body.appendChild(el);	 
+}; grabitl(); void(0);
+
+javascript:function%20grabitl(){if(!document.body){alert('not_loaded_yet');return;}var%20el=document.createElement('scr'+'ipt');el.setAttribute('src','http://grabitl/process.php?h=haarer0&p='+encodeURIComponent(document.location.href)+'&t='+(new%20Date().getTime()));document.body.appendChild(el);};grabitl();void(0);
